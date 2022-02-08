5bade72f86eb9f53ed3e243206e33a6475a6fa32
Aubrey Patsika <jangili@jangili-mac.myfiosgateway.com>
2022-02-08 16:34:38 -0500

    New border radius

--- components/Card.js
+++ components/Card.js
@@ -6,7 +6,7 @@ const Card = ({ title, description, imgSrc, href }) => (
     <div
       className={`${
         imgSrc && 'h-full'
-      }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
+      }  overflow-hidden rounded-md border-4 border-indigo-500/75`}
     >
       {imgSrc &&
         (href ? (
