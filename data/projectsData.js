0e0e0d43fc74b708d477bfd0a320cc99f650f029
Aubrey Patsika <jangili@jangili-mac.myfiosgateway.com>
2022-02-08 17:08:48 -0500

    new blog

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,15 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Grand Canyon',
+    description: `Nothing could have prepared us for the beauty of the canyons and now we can understand why they are one of the most difficult place to photograph`,
+    imgSrc: '/static/images/grand-canyon/antelope.jpg',
+    href: '/blog/grand-canyon',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Vercel Web Project',
+    description: `Vercel is a JavaScript framework built on top on REACT. In this blog we will look at how I have architected my solution`,
+    imgSrc: '/static/images/vercel.png',
+    href: '/blog/vercel-project',
   },
 ]
 
