60f2531c6a75bbe387440360c44b1c019d26262c
Aubrey Patsika <jangili@jangili-mac.myfiosgateway.com>
2022-02-08 21:05:49 -0500

    Develop preview ship

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,15 +1,15 @@
 const projectsData = [
   {
-    title: 'Grand Canyon',
-    description: `Nothing could have prepared us for the beauty of the canyons and now we can understand why they are one of the most difficult place to photograph`,
-    imgSrc: '/static/images/grand-canyon/antelope.jpg',
-    href: '/blog/grand-canyon',
-  },
-  {
     title: 'Vercel Web Project',
     description: `Vercel is a JavaScript framework built on top on REACT. In this blog we will look at how I have architected my solution`,
     imgSrc: '/static/images/vercel.png',
     href: '/blog/vercel-project',
+  },
+  {
+    title: 'Grand Canyon',
+    description: `Nothing could have prepared us for the beauty of the canyons and now we can understand why they are one of the most difficult place to photograph`,
+    imgSrc: '/static/images/grand-canyon/antelope.jpg',
+    href: '/blog/grand-canyon',
   },
 ]
 
